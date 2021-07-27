cd7540bf2e7441d511bf5a52d9077fbbfd8c4dca
Robert Schwartz <bschwartz23@gmail.com>
2021-07-26 17:24:21 -0700

    set up Projects page main block

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,18 @@
 import React from "react";
+import ProjectCard from "../components/ProjectCard"
 
 function Projects() {
-  return <div> Projects text</div>;
+  return (
+    <div>
+      <h1> Recent Projects</h1>
+      <h2>
+        Here are some Projects that I have worked on
+      </h2>
+      <div className="container">
+        <ProjectCard />
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
